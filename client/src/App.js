0bd0e7f68a8091426fcd147fae3da977ae3dbd03
Ben Dixon <bendixon311@gmail.com>
2019-04-11 21:07:42 -0600

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React - attempting to connect frontend to my backend.
-          </a>
-        </header>
+       <Header />
+          <Switch>
+            <Route
+                //add route here!
+            ></Route>
+          </Switch>
+
+       <Footer />
       </div>
     );
   }
