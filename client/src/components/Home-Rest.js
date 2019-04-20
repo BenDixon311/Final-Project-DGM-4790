c973d2c89027750da7af78531a9e8e6136daec7e
Ben Dixon <bendixon311@gmail.com>
2019-04-20 17:25:56 -0600

    added cards (img not working)

--- client/src/components/Home-Rest.js
+++ client/src/components/Home-Rest.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { classExpression } from '@babel/types';
+
+const styles = {
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  };
 
 
 
@@ -9,6 +28,9 @@ class HomeRest extends Component{
     state = {
         playerData: []
     }
+
+    
+   
 
     componentDidMount() {
         axios.get(proxyurl + 'https://restful-crud-node-server.herokuapp.com/')
@@ -22,11 +44,42 @@ class HomeRest extends Component{
     render() {
         return (
             <div>
-                <p>Hey whats up!</p>
-                <ul>{ this.state.playerData.map(player => <li>Player Name: { player.name } Team: { player.team }</li>)}</ul>
+                { this.state.playerData.map(player =>
+                    <Card>
+                    <CardActionArea>
+                        <CardMedia
+                         
+                          image={player.imgurl}
+                          title={player.name}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h4" component="h2">
+                                {player.name}
+                            </Typography>
+                            <Typography component="h4">
+                                {player.team}
+                            </Typography>
+                            <Typography component="h5">
+                                {player.position}
+                            </Typography>
+
+                        </CardContent>
+
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                        Learn More
+                        </Button>
+                    </CardActions>
+                </Card>
+
+
+                )}
             </div>
         )
     }
 }
+
+
 
 export default HomeRest;
