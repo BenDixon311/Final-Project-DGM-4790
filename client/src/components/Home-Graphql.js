15e4a4cd28ad50eb4e5c13e421666a56df538c92
Ben Dixon <bendixon311@gmail.com>
2019-04-22 21:50:14 -0600

    connects to graphql server

--- client/src/components/Home-Graphql.js
+++ client/src/components/Home-Graphql.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
 
 class HomeGraphql extends Component{
     render() {
         
+        const QUERY = gql`
+        {
+        projects {
+            title
+            description
+            id
+            devtype
+          }
+         }
+        `
 
         return (
-            <div>
+            <Query query={QUERY}>
+                {({ loading, error, data }) => {
+                 if (loading) return <div>Fetching</div>;
+                if (error) return <div>Error</div>;
 
-            </div>
+          return (
+                <div>
+                
+                {data.projects.map(project => (
+                    <p>{project.title}</p>
+                    
+                ))}
+
+                </div>
+          )
+                }
+            }
+            </Query>
+            
         )
     }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000',
+});
+
+
+
+const client = new ApolloClient({
+     link: httpLink,
+     cache: new InMemoryCache(),
+    
+  });
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </BrowserRouter>
     
     
